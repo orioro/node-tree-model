2e32d6d50d5369e32b570f9ed4df5f3770e3109f
Simon Fan <simon@orioro.design>
2021-08-22 12:30:48 -0300

    feat: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import * as traversal from './traversal'
+import * as structure from './structure'
+import * as util from './util'
+
 export * from './types'
 export * from './treeModel'
+export { traversal, structure, util }
